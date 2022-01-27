668785191d42ed4accaac7a9a86d9b6e14b9500c
aymanab5 <aymansce@gmail.com>
2022-01-27 17:29:41 +0200

    add new proccess in the server and student and fix bugs in mobx of admin

--- src/stores/AdminInventory.js
+++ src/stores/AdminInventory.js
@@ -1,11 +1,11 @@
 import { observable, action, makeObservable, computed } from "mobx";
 import axios from "axios";
-import { toJS } from 'mobx'
+import { toJS } from "mobx";
 
 export class AdminInventory {
   constructor() {
     this.AdminJobs = [];
-    this.allStudents = []
+    this.allStudents = [];
     makeObservable(this, {
       AdminJobs: observable,
       allStudents: observable,
@@ -19,23 +19,23 @@ export class AdminInventory {
   }
 
   sortAllStudentJobs = () => {
-    this.allStudents.map(s => {
-      s.jobs.map(j => {
-        let newJob = j
-        newJob['studentName'] = s.firstName + ' ' + s.lastName
-        newJob['mobileNo'] = s.mobileNo
-        newJob['email'] = s.email
-        newJob['cohort'] = s.cohort
-        this.AdminJobs.push(newJob)
-      })
-    })
-  }
+    this.allStudents.map((s) => {
+      s.jobs.map((j) => {
+        let newJob = j;
+        newJob["studentName"] = s.firstName + " " + s.lastName;
+        newJob["mobileNo"] = s.mobileNo;
+        newJob["email"] = s.email;
+        newJob["cohort"] = s.cohort;
+        this.AdminJobs.push(newJob);
+      });
+    });
+  };
 
   addJobsFromDBToAdmin = async () => {
-    // this.allStudents = []
+    this.allStudents = [];
+    this.AdminJobs = [];
     let result = await axios.get(`http://localhost:8888/admin/jobs`);
     this.allStudents = result.data;
-    this.sortAllStudentJobs()
-
+    this.sortAllStudentJobs();
   };
 }
