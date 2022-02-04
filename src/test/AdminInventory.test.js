5337d1d75d66d83dad045aa78f28c87677bf0491
aymanab5 <aymansce@gmail.com>
2022-02-04 22:49:49 +0200

    Testing-jest: number Of Students in the beginning

--- src/test/AdminInventory.test.js
+++ src/test/AdminInventory.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { AdminInventory } from "../stores/AdminInventory";
 
-test("Testing AdminInventory", () => {
+test("Testing number Of Students in the beginning", () => {
   const adminInventory = new AdminInventory();
   expect(adminInventory.numOfStudents).toBe(0);
 });
