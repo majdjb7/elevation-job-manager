0966a041aacc3e06af57e66a26a513443d8cb6c4
mostfa <mostfa13392@gmail.com>
2022-02-07 18:55:44 +0200

    style login page

--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -1,5 +1,33 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom'
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://elevation.ac/he/?placement&utm_term=elevation&utm_campaign=Elevation%20-%20Brand&utm_source=google&utm_medium=cpc&hsa_acc=4465174973&hsa_net=adwords&hsa_cam=6553671782&hsa_ad=512242236400&hsa_kw=elevation&hsa_grp=80166385658&hsa_mt=e&hsa_ver=3&hsa_src=g&hsa_tgt=kwd-13311466&gclid=CjwKCAiAo4OQBhBBEiwA5KWu_x8hZtZ9X-XVniq9OWe4tQ4YzvcmzT_QXR0pnTUe87gJXrOZbynPXBoCPn8QAvD_BwE">
+                Elevation
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
 
 export default function Register() {
     const [firstName, setFirstName] = useState('');
@@ -12,10 +40,10 @@ export default function Register() {
 
     const submit = async (e) => {
         e.preventDefault();
-        
+
         await fetch('http://localhost:8888/auth/register', {
             method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 firstName,
                 lastName,
@@ -28,12 +56,121 @@ export default function Register() {
         setRedirect(true)
     }
 
-    if(redirect) {
-        return <Redirect to="/"/>;
+    if (redirect) {
+        return <Redirect to="/" />;
     }
 
-  return (
-    <form onSubmit={submit}>
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: '#2e70ff' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign up
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={submit} sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        autoComplete="given-name"
+                                        name="firstName"
+                                        required
+                                        fullWidth
+                                        id="firstName"
+                                        label="First Name"
+                                        onChange={e => setFirstName(e.target.value)}
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="lastName"
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="family-name"
+                                        onChange={e => setLastName(e.target.value)}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        onChange={e => setEmail(e.target.value)}
+                                    />
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            name="password"
+                                            label="Password"
+                                            type="password"
+                                            id="password"
+                                            autoComplete="new-password"
+                                            onChange={e => setPassword(e.target.value)}
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="phone"
+                                        label="Phone Number"
+                                        name="phone"
+                                        autoComplete="phone"
+                                        onChange={e => setMobileNo(e.target.value)}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="cohort"
+                                        label="Cohort"
+                                        name="cohort"
+                                        onChange={e => setCohort(e.target.value)}
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justifyContent="flex-end">
+                                <Grid item>
+                                    <Link href="/Login" variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 5 }} />
+                </Container>
+            </ThemeProvider>
+
+            {/* <form onSubmit={submit}>
         <h1 className="h3 mb-3 fw-normal">Please register</h1>
         
         <input className="form-control" placeholder="First Name" required
@@ -62,7 +199,7 @@ export default function Register() {
 
         <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
 
-    </form>
-    
-  )
+    </form> */}
+        </>
+    )
 }
