918432dd2a5f1fdab717aea2d2506241d04450ec
majdjb7 <majdjd171@gmail.com>
2022-02-08 23:40:34 +0200

    fix heroku 7.0

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors())
 
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://elevation-job-manager.herokuapp.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
